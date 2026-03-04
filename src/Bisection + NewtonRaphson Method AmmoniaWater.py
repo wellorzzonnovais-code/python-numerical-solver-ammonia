cde59bef96c73e5fd9dc0dfc26ae6870419588e3
Wellorzzon Novais <well.orz.zon@gmail.com>
2026-03-04 00:24:57 -0300

    Update root-finding modules and integrate useful_tool dependency

--- src/Bisection + NewtonRaphson Method AmmoniaWater.py
+++ src/Bisection + NewtonRaphson Method AmmoniaWater.py
@@ -1,10 +1,144 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
 
-This is a temporary script file.
+Find zero point using Bisection (first) and Newton-Raphson (second) Method.
+
 """
 
+
+import iapws
+import modules.useful_tools as ut
+
+import time
+start_time = time.time() # Measuring above 0.1 seconds
+
+
+P_target = 15.0*ut.pressure_bar_to_Pa # [Pa]
+T_target = 273.15+120 # [K]
+xmass_target = 0.42 # [kg NH3/kg H2O]
+MW_NH3 = 17.03026 # [g/mol] referência do iapws, no docuemnto de nh3h2o
+MW_H2O = 18.015268 # [g/mol] referência do iapws, no docuemnto de nh3h2o
+xmol_target = ut.xmass_to_xmol(xmass_target,MW_NH3,MW_H2O) # [mol/mol]
+
+rho_guess = [0.1,25.1] # [kg/m³] [6.369055814002477,20.536754868000003] zeros of 11 bar, 132°C and xmass = 0.42
+
+n = 10 # number to divide guess range
+tol_BS = pow(10,-1) # Tolerance to zero for Bisection Method
+tol_NR = pow(10,-7) # Tolerance to zero for Newton-Raphson Method
+N = 3000 # limit for numbers of iterations
+
+
+print("For:")
+print("P = " + str(P_target) + " Pa")
+print("T = " + str(T_target) + " K")
+print("x_mass = " + str(xmass_target) + " (mass fraction of ammonia)")
+print("rho guess = " + str(rho_guess) + " kg/m³")
+print("")
+
+
+
+def function_P_rho(rho):
+    try:
+        function = (iapws.ammonia.H2ONH3()._prop(rho, T_target, xmol_target).get("P") * ut.pressure_MPa_to_Pa) - P_target
+        return function
+    except:
+        return print("Error in guess = " + str(rho) + ". \n")
+
+
+x_ranges = ut.divide_into_ranges(rho_guess[0],rho_guess[1],n)
+# print(x_ranges)
+function_ranges = []
+for x_min, x_max in x_ranges:
+    function_ranges.append([function_P_rho(x_min), function_P_rho(x_max)])
+# print(function_ranges)
+x_zero_ranges = ut.x_with_zero_ranges(x_ranges, function_ranges, tol_NR)
+# print(ut.x_with_zero_ranges(x_ranges, function_ranges, tol))
+
+
+
+# Bisection Method
+#-----------------------------------
+
+def find_zero_bisection_rho(guess_min, guess_max, tolerance, limit_iterations):
+    i = 1
+    while i <= limit_iterations:
+        guess_avg = guess_min + (guess_max - guess_min)/2
+        function_guess_min = function_P_rho(guess_min) # [bar]
+        function_guess_avg = function_P_rho(guess_avg) # [bar]
+        if function_guess_avg == 0 or abs((guess_min - guess_max)/2) < tolerance:
+            return guess_avg
+        elif function_guess_min*function_guess_avg > 0:
+            guess_min = guess_avg
+        else:
+            guess_max = guess_avg
+        i += 1
+        
+    if i > N:
+        return print("Method failed after " + str(i-1) + " iteration(s).")
+
+
+# Check if I have a range or zero points, and apply bisection method just within ranges
+x_with_zero = []
+for x in x_zero_ranges:
+    if not(isinstance(x, list)):
+        x_with_zero.append(x)
+    else:
+        x_zero = find_zero_bisection_rho(x[0],x[1],tol_BS,N)
+        x_with_zero.append(x_zero)
+
+
+def find_zero_newtonraphson_rho(guess, tolerance, limit_iterations):
+    i = 1
+    while i <= N:
+        guess_minusStep = guess*(1-tolerance) # tolerance used to estimate differential curve
+        guess_plusStep = guess*(1+tolerance)
+        if guess_minusStep <= 0 or guess_plusStep <= 0:
+            return print("Error! Guess diverged after " + str(i) + " iterations. Check the input conditions.")
+        
+        differential_function = (function_P_rho(guess_plusStep)-function_P_rho(guess_minusStep))/(guess_plusStep-guess_minusStep)
+        guess_now = guess-function_P_rho(guess)/differential_function
+        if abs(guess_now-guess) < tolerance:
+            return guess_now #print("guess = "+ str(guess_now)+", after "+str(i)+" iterations.")
+        else:
+            guess = guess_now
+            i += 1
+            
+    if i > N:
+        return print("Method failed after " + str(i-1) + " iteration(s).")
+
+     
+x_zeros = []
+for x in x_with_zero:
+    x_zero = find_zero_newtonraphson_rho(x, tol_NR, N)
+    x_zeros.append(x_zero)
+    
+# Print answers
+if x_zeros == []:
+    print("Those guesses does not have a zero point or diverged. Try refine the range or change values for guesses.")
+else:
+    print("we have zero with guess = ")
+    print(x_zeros)
+
+
+executionTime = (time.time() - start_time)
+print("--- %s seconds ---" % executionTime)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
 def xmass_to_xmol(xmass,mass_weight1,mass_weight2):
     xmol_converted = (xmass/mass_weight1)/(xmass/mass_weight1 + (1-xmass)/mass_weight2)
     return xmol_converted
@@ -143,13 +277,4 @@ while j <= N_0j:
 if j > N_0j:
     print("Method failed after " + str(j-1) + " iteration(s).")
 
-
-
-
-
-
-
-
-
-
-
+"""
--- src/Search guess divided.py
+++ src/Search guess divided.py
@@ -4,7 +4,6 @@ This tool divides a functions into n parts and find a zero point between those.
 Can be used as a first guess to others functions
 
 # Restrictoins or To-do:
-    1. Does not cover two zeros between the guesses range;
     2. Does not address outside functions, yet;
     3. Does not return more than just the range (could find the zero point itself).
     
@@ -14,23 +13,17 @@ import modules.useful_tools as useful_tools
 import iapws
 
 
-Tol_BS = pow(10,-3) #Tolerance bisection method
-
-P_target = 11.0*useful_tools.pressure_bar_to_Pa # [Pa]
-#print(P_target)
-T_target = 273.15+130 # [K]
+P_target = 15.0*useful_tools.pressure_bar_to_Pa # [Pa]
+T_target = 273.15+120 # [K]
 xmass_target = 0.42 # [kg NH3/kg H2O]
 MW_NH3 = 17.03026 # [g/mol] referência do iapws, no docuemnto de nh3h2o
 MW_H2O = 18.015268 # [g/mol] referência do iapws, no docuemnto de nh3h2o
 xmol_target = useful_tools.xmass_to_xmol(xmass_target,MW_NH3,MW_H2O) # [mol/mol]
 
-#rho_guess_hand = 0.01 # [kg/m³]
-rho_guess = [20, 50] # [kg/m³]
-#rho_guessBi = 500 # [kg/m³]
-step_rho = 1.0 # [kg/m³]
-step_diff_rho = 0.0001
+rho_guess = [0.1,25.1] # [kg/m³] [6.369055814002477,20.536754868000003] zeros of 11 bar, 132°C and xmass = 0.42
 
-n = 100
+n = 100 # number to divide guess range
+tol = pow(10,-7) # Tolerance to zero
 
 
 def divide_into_ranges(x_min, x_max, n_divisions):
@@ -45,10 +38,6 @@ def divide_into_ranges(x_min, x_max, n_divisions):
     return list
 
 
-x_ranges = divide_into_ranges(rho_guess[0],rho_guess[1],n) # (7.427516, 7.427518, 100)
-# for x in x_ranges:
-#     print(x)
-
 def function_P_rho(rho):
     try:
         function = (iapws.ammonia.H2ONH3()._prop(rho, T_target, xmol_target).get("P") * useful_tools.pressure_MPa_to_Pa) - P_target
@@ -56,47 +45,37 @@ def function_P_rho(rho):
     except:
         return print("Error in guess = " + str(rho) + ". \n")
 
-function_ranges = []
 
-for x_min, x_max in x_ranges:
-    function_ranges.append([function_P_rho(x_min), function_P_rho(x_max)])
-    
-
-# print()
-# print(function_ranges)
-
-# for x in function_ranges:
-#     print(x)
-
-# def test_zeros(list_guesses, list_functions):
-
-def test_function_zero(x_ranges, function_ranges, tol): 
-    
+def function_zeros_range(x_ranges, function_ranges, tolerance): 
     function_ranges_with_zero = []
+    guess_with_zero = []
     i = 0
     for f in function_ranges:
         i += 1
         if (f[0]*f[1] < 0):
             function_ranges_with_zero.append(f) # ATTENTION: cannot add multiple rho ranges, even using append list
             print()
-            print("There is a zero in " + str(x_ranges[i-1]) + ", function between " + str(function_ranges_with_zero) + ".")
+            print("There is a zero in " + str(x_ranges[i-1]))
+            print("with function between " + str(f) + ".")
         elif abs(f[0]) <= tol:
-            guess_with_zero = x_ranges[i-1][0]
+            guess_with_zero.append(x_ranges[i-1][0])
             print()
             print("Guess with function equal to zero: " + str(guess_with_zero) + ".")
-            break
         elif abs(f[1]) <= tol:
-            guess_with_zero = x_ranges[i-1][1]
+            guess_with_zero.append(x_ranges[i-1][1])
             print()
             print("Guess with function equal to zero: " + str(guess_with_zero) + ".")
-            break
     
     if 'guess_with_zero' in locals():
-        return print("\n" + str(guess_with_zero))
+        return guess_with_zero
     else:
-        return print("\n" + str(function_ranges_with_zero))
+        return function_ranges_with_zero
 
 
-test_function_zero(x_ranges, function_ranges, Tol_BS)
+x_ranges = divide_into_ranges(rho_guess[0],rho_guess[1],n)
+function_ranges = []
+for x_min, x_max in x_ranges:
+    function_ranges.append([function_P_rho(x_min), function_P_rho(x_max)])
+function_zeros_range(x_ranges, function_ranges, tol)
 
 
